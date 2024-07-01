b8d413131008e3445eed9d709f278cd41c76f1d8
ladydi100 <lady.tituana@hotmail.com>
2024-07-01 08:05:00 -0500

    Funcionalidad Login Lady Tituana

---
+++ javascript/java_login.js
@@ -0,0 +1,60 @@
+const form = document.querySelector("form");
+eField = form.querySelector(".email"),
+eInput = eField.querySelector("input"),
+pField = form.querySelector(".password"),
+pInput = pField.querySelector("input");
+
+form.onsubmit = (e)=>{
+  e.preventDefault(); 
+
+  (eInput.value == "") ? eField.classList.add("shake", "error") : checkEmail();
+  (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
+
+  setTimeout(()=>{ 
+    eField.classList.remove("shake");
+    pField.classList.remove("shake");
+  }, 500);
+
+  eInput.onkeyup = ()=>{checkEmail();} 
+  pInput.onkeyup = ()=>{checkPass();} 
+
+  function checkEmail(){ 
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; 
+    if(!eInput.value.match(pattern)){ 
+      eField.classList.add("error");
+      eField.classList.remove("valid");
+      let errorTxt = eField.querySelector(".error-txt");
+   
+      (eInput.value != "") ? errorTxt.innerText = "Ingrese un correo valido" : errorTxt.innerText = "Correo no puede estar en blanco";
+    }else{ 
+      eField.classList.remove("error");
+      eField.classList.add("valid");
+    }
+  }
+
+  function checkPass(){ 
+    if(pInput.value == ""){ 
+      pField.classList.add("error");
+      pField.classList.remove("valid");
+    }else{ 
+      pField.classList.remove("error");
+      pField.classList.add("valid");
+    }
+  }
+
+  //validacion exitosa
+  if(!eField.classList.contains("error") && !pField.classList.contains("error")){
+   const ema = document.getElementById("emails").value;
+   sessionStorage.setItem('email', ema);
+
+  
+   window.location.href = "login1.html";
+   
+   
+    
+/*
+  
+    document.getElementById('precio').textContent  = localStorage.getItem('email');*/
+  }
+}
+
