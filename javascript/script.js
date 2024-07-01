f7955675bf905bc2bc51bd98b0e8e64b15607b70
NachoCardenas2012 <nacho_cgsp@hotmail.com>
2024-07-01 11:31:39 -0500

    cambio-script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+    let cards = document.querySelectorAll('.card');
+    cards.forEach(function(card) {
+        card.style.display = 'block';
+    });
+
     document.getElementById('searchForm').addEventListener('submit', function(event) {
         event.preventDefault();
 
         let searchTerm = document.getElementById('searchInput').value.toLowerCase().trim();
 
-        let cards = document.querySelectorAll('.card');
-
-        // Ocultar todas las tarjetas
-        cards.forEach(function(card) {
-            card.style.display = 'none';
-        });
-
-        // Mostrar la tarjeta que coincide con el término de búsqueda
-        cards.forEach(function(card) {
-            let title = card.querySelector('.card-title').textContent.toLowerCase();
-
-            if (title.includes(searchTerm)) {
+        if (searchTerm === "") {
+            cards.forEach(function(card) {
                 card.style.display = 'block';
-                card.classList.add('text-center'); // Centrar la tarjeta encontrada
-            }
-        });
+                card.classList.remove('text-center');
+            });
+        } else {
+            cards.forEach(function(card) {
+                let title = card.querySelector('.card-title').textContent.toLowerCase();
+
+                if (title.includes(searchTerm)) {
+                    card.style.display = 'block';
+                    card.classList.add('text-center');
+                } else {
+                    card.style.display = 'none';
+                }
+            });
+        }
     });
 });
